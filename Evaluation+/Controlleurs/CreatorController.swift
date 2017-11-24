8e135a4d93622b9ae37c792602d6c0ebb09dbe58
João Carlos Fernandes Neto <wysrar@gmail.com>
2017-11-24 14:08:11 -0500

    v1.0

--- Evaluation+/Controlleurs/CreatorController.swift
+++ Evaluation+/Controlleurs/CreatorController.swift
@@ -1,38 +1,104 @@
 //
+//  GrandeController.swift
+//  cours_13
 //
-//
+//  Created by João Carlos Fernandes Neto on 17-11-24.
+//  Copyright © 2017 João Carlos Fernandes Neto. All rights reserved.
 //
 import UIKit
-//--- ** ---\\
-class CreatorController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextViewDelegate {
-    //--- * Conection * ---\\
-    @IBOutlet weak var criterialTextView: UITextView!
-    @IBOutlet weak var criterialTextField: UITextField!
-    @IBOutlet weak var criterialTableView: UITableView!
-    //--- * Classe UserDefaultsManager * ---\\
-    let saveDataObj = UserDefaultsManager()
-    //--- * Variables * ---\\
-    var criteriasDictionary: [String: [String]]!
-    //--- ** ---\\
+import Foundation
+//---\\      *      //---\\
+class CreatorController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+    //---\\      *      //---\\
+    let userDefaultsObj = UserDefaultsManager()
+    //---\\      *      //---\\
+    @IBOutlet weak var grandeField: UITextField!
+    @IBOutlet weak var couseField: UITextField!
+    @IBOutlet weak var student_name_label: UILabel!
+    @IBOutlet weak var course_grande_tableveiw: UITableView!
+    //---\\      *      //---\\
+    typealias studentName = String
+    typealias couseName = String
+    typealias gradeCouse = Double
+    //---\\      *      //---\\
+    var studentGredes: [studentName: [couseName: gradeCouse]]!
+    var arrayOfCourse: [couseName]!
+    var arrayOfGrades: [gradeCouse]!
+    //---\\      *      //---\\
     override func viewDidLoad() {
         super.viewDidLoad()
-        //loadSavedData()
+        student_name_label.text = userDefaultsObj.getValue(theKey: "name") as? String
+        loadUserDefaults()
+        fillUpArray()
     }
-    //--- * TableView Controller * ---\\
+    //---\\      *      //---\\
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return arrayOfCourse.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = UITableViewCell(style:
-            UITableViewCellStyle.default, reuseIdentifier: nil)
+        let cell: UITableViewCell = course_grande_tableveiw.dequeueReusableCell(withIdentifier: "proto")!
+        if let aCourse = cell.viewWithTag(100) as! UILabel! {
+            aCourse.text = arrayOfCourse[indexPath.row]
+        }
+        if let aGrande = cell.viewWithTag(101) as! UILabel! {
+            aGrande.text = String(arrayOfGrades[indexPath.row])
+        }
         return cell
     }
-    //--- * End TableView Controller * ---\\
-    //--- ** ---\\
-    //--- * Method loadSavedData() for save user data * ---\\
-    //func loadSavedData() {
-     //
-    //}
-    //--- ** ---\\
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let name = [studentName](studentGredes.keys)[indexPath.row]
+        userDefaultsObj.setKey(theValue: name as AnyObject, theKey: "name")
+        performSegue(withIdentifier: "proto", sender: nil)
+    }
+    //---\\      *      //---\\
+    func fillUpArray() {
+        let name = student_name_label.text
+        let couses_and_grands = studentGredes[name!]
+        arrayOfCourse = [couseName](couses_and_grands!.keys)
+        arrayOfGrades = [gradeCouse](couses_and_grands!.values)
+    }
+    //---\\      *      //---\\
+    func loadUserDefaults() {
+        if userDefaultsObj.doesKeyExist(theKey: "gradeCouse") {
+            studentGredes = userDefaultsObj.getValue(theKey: "gradeCouse") as! [studentName: [couseName: gradeCouse]]
+        } else {
+            studentGredes = [studentName: [couseName: gradeCouse]]()
+        }
+    }
+    //---\\      *      //---\\
+    @IBAction func addCourseAndGrande(_ sender: UIButton) {
+        let name = student_name_label.text!
+        var student_courses = studentGredes[name]!
+        student_courses[couseField.text!] = gradeCouse(grandeField.text!)
+        studentGredes[name] = student_courses
+        userDefaultsObj.setKey(theValue: studentGredes as AnyObject, theKey: "gradeCouse")
+        fillUpArray()
+        course_grande_tableveiw.reloadData()
+    }
+    //---\\      *      //---\\
 }
-//--- ** ---\\
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Evaluation+/Controlleurs/MainController.swift
+++ Evaluation+/Controlleurs/MainController.swift
@@ -1,15 +1,78 @@
 //
+//  ViewController.swift
+//  cours_13
 //
+//  Created by João Carlos Fernandes Neto on 17-11-24.
+//  Copyright © 2017 João Carlos Fernandes Neto. All rights reserved.
 //
-//
+
 import UIKit
-//--- ** ---\\
-class MainController: UIViewController {
-    //--- * Classe UserDefaultsManager * ---\\
-    let userDefaults = UserDefaultsManager()
-    //--- ** ---\\
+
+class MainController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+    //---\\      *      //---\\
+    @IBOutlet weak var student_name_tableview: UITableView!
+    @IBOutlet weak var student_name_Fild: UITextField!
+    //---\\      *      //---\\
+    let userDefaultsObj = UserDefaultsManager()
+    //---\\      *      //---\\
+    typealias studentName = String
+    typealias couseName = String
+    typealias gradeCouse = Double
+    //---\\      *      //---\\
+    var studentGredes: [studentName: [couseName: gradeCouse]]!
+    //---\\      *      //---\\
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadUserDefaults()
+    }
+    //---\\      *      //---\\
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return studentGredes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: nil)
+        cell.textLabel?.text = [studentName](studentGredes.keys)[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, commit editinsStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editinsStyle == UITableViewCellEditingStyle.delete {
+            let name = [studentName](studentGredes.keys)[indexPath.row]
+            studentGredes[name] = nil
+            userDefaultsObj.setKey(theValue: studentGredes as AnyObject, theKey: "gradeCouse")
+            tableView.deleteRows(at: [indexPath as IndexPath], with: UITableViewRowAnimation.automatic)
+        }
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let name = [studentName](studentGredes.keys)[indexPath.row]
+        userDefaultsObj.setKey(theValue: name as AnyObject, theKey: "name")
+        performSegue(withIdentifier: "seg", sender: nil)
+    }
+    //---\\      *      //---\\
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    //---\\      *      //---\\
+    func loadUserDefaults() {
+        if userDefaultsObj.doesKeyExist(theKey: "gradeCouse") {
+            studentGredes = userDefaultsObj.getValue(theKey: "gradeCouse") as! [studentName: [couseName: gradeCouse]]
+        } else {
+            studentGredes = [studentName: [couseName: gradeCouse]]()
+        }
+    }
+    //---\\      *      //---\\
+    @IBAction func addstudent(_ sender: UIButton) {
+        if student_name_Fild.text != "" {
+            studentGredes[student_name_Fild.text!] = [couseName: gradeCouse]()
+            student_name_Fild.text = ""
+            userDefaultsObj.setKey(theValue: studentGredes as AnyObject, theKey: "gradeCouse")
+            student_name_tableview.reloadData()
+        }
     }
 }
-//--- ** ---\\
+//---\\=======(*)=======//---\\
+
+
+
+
